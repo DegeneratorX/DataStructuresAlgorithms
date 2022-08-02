43ff9fa3bfff8279287d8baf15a7ff5d8ac5215a
Victor <victorio.deg@gmail.com>
2022-08-01 21:01:49 -0300

    Intro de lista encadeada

---
+++ aula2-linkedlist/linkedlist1_intro.py
@@ -0,0 +1,10 @@
+"""
+Uma lista encadeada não possui índices.
+
+Cada valor dentro dessa lista está próximo um do outro na memória.
+
+Uma lista encadeada possui:
+- nós (nodes), que apontam pro próximo elemento da lista (aponta pra None se último.)
+- variável cabeça (head) que aponta pro primeiro elemento da lista
+- variável cauda (tail) que aponta pro último elemento da lista
+"""
